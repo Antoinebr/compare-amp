7782d53929f37dfdf8b2cf156843ad308467c8e1
Antoine Brossault <antoinebrossault@gmail.com>
2019-04-09 17:41:12 +0100

    refactoring + error handeling

---
+++ src/js/amp-and-cannonical-detector.js
@@ -0,0 +1,46 @@
+
+
+const isURL = require('validator/lib/isURL');
+
+/**
+ * @name getAlternativeURL
+ * @summary return the AMP URL from the cannonical URL or the opposite (this function should be used on front-end only)
+ * @param {string} sourceURL URL ( could be the cannonical URL or the AMP url)
+ * @returns {Promise || error} the promise will contain the alternative URL 
+ */
+exports.getAlternativeURL = async (sourceURL,linkRelType) => {
+
+	let alternativeURL = null;
+
+    if(typeof sourceURL !== "string"){
+        throw new Error(`We expected a string as sourceURL we got ${typeof sourceURL}`);
+    }
+
+    if (!isURL(sourceURL)) {
+        throw new Error(`We expected an URL we got ${sourceURL}`);
+	}
+	
+	if( typeof linkRelType !== "string"){
+		throw new Error(`We expected a string as linkRelType we got ${typeof linkRelType}`);
+	}
+
+	//Using cors-anywhere API to fetch the URL without origin issues
+	const fetchedPage = await fetch(`https://cors-anywhere.herokuapp.com/${sourceURL}`, {
+		mode: 'cors'
+	});
+
+	if (!fetchedPage.ok) throw new Error(`We received an invalid response ${await fetchedPage.text()}`);
+
+	const fetchedPageTextReesponse = await fetchedPage.text();
+
+	const parser = new DOMParser();
+	const htmlDocument = parser.parseFromString(fetchedPageTextReesponse, "text/html");
+
+	alternativeURL = htmlDocument.documentElement.querySelector(`link[rel="${linkRelType}"`).getAttribute("href");
+
+	if( !isURL(alternativeURL) ){
+		throw new Error(`We couldn't find a valid alternativeURL we got ${alternativeURL}`);
+	}
+
+	return alternativeURL;
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
 import css from '@css/app.scss'
+
+import {
+	getAlternativeURL
+} from './amp-and-cannonical-detector';
+
 /**
  * @name testIframes
  * @description Test if a given URL can be iframed
@@ -81,206 +86,203 @@ const toggleCompareButton = () => {
 	}
 }
 
-const prepareDetection = async (currentField, otherField, tagToFind) => {
+
+/**
+ * @name tryToGetAlternateURL
+ * @description Try to inject in the page the alternative URL form a given URL
+ * @param {string} sourceURL should be a valid URL
+ * @param {HTMLElement} alternateURLField an existing HTML element
+ * @param {string} linkRelType the value of link rel "amphtml" "cannonical"...
+ */
+const tryToGetAlternateURL = async (sourceURL, alternateURLField, linkRelType) => {
 	//remove any existing detector buttons
 	if (document.querySelector('.detector') !== null) {
-		var detector = document.querySelector('.detector');
+		const detector = document.querySelector('.detector');
 		detector.parentNode.removeChild(detector);
 	}
-	//show a detector button on the other field, if the currentfield has a valid URL
-	let isURL = await import('validator/lib/isURL');
-	isURL = isURL.default;
-	if (isURL(currentField.value)){
-		
-		otherField.previousElementSibling.insertAdjacentHTML('beforeend','<a href="#" class="detector"> - Detect?</a>');
-		var newDetector = document.querySelector('.detector');
-		newDetector.addEventListener('click', async function(e){
-			e.preventDefault();
-			this.disabled=true;
-			this.innerHTML=" - Detecting...";
-			var otherURL = await detectOtherUrl(currentField, otherField,tagToFind);
-			if (otherURL){
-				otherField.value = otherURL;
-				this.parentNode.removeChild(this);
-			}else{
-				this.innerHTML=" - Detection failed. Try again?";
-				this.disabled = true;
-			}
-		});
-	}
+
+	alternateURLField.previousElementSibling.insertAdjacentHTML('beforeend', '<a href="#" class="detector"> - Detect?</a>');
+	const newDetector = document.querySelector('.detector');
+
+	newDetector.addEventListener('click', async function (e) {
+
+		e.preventDefault();
+		this.disabled = true;
+		this.innerHTML = " - Detecting...";
+
+		const ampURL = await getAlternativeURL(sourceURL, linkRelType).catch(e => {
+			this.innerHTML = " - Detection failed. Try again?";
+			this.disabled = true;
+			console.error(e)
+		})
+
+		alternateURLField.value = ampURL
+
+		this.parentNode.removeChild(this);
+
+	});
+
 }
 
-const detectOtherUrl = async (currentField, otherField, tagToFind) => {
-	
-	var detectedURL = false;
-	var parsedURL = "";
-
-	try{
-		//Using cors-anywhere API to fetch the URL without origin issues
-		var fetchedPage = await fetch('https://cors-anywhere.herokuapp.com/'+currentField.value, {mode:'cors'})
-		.then( response => response.text())
-		.then( text => {
-			const parser = new DOMParser();
-			const htmlDocument = parser.parseFromString(text, "text/html");
-			try{
-				parsedURL = htmlDocument.documentElement.querySelector("link[rel='"+tagToFind+"']").getAttribute("href");
-			}catch(error){
-				return detectedURL;
-			}
-			detectedURL = parsedURL;
-		});
-	}catch{
-		return detectedURL;
-	}
-
-	return detectedURL;
-}
 
 const $checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
 // Allow only one checkbox to be ticked 
-$checkboxes.forEach(elemenent => elemenent.addEventListener('click',function(e){
+$checkboxes.forEach(elemenent => elemenent.addEventListener('click', function (e) {
 
 	$checkboxes.forEach(elemenent => elemenent.checked = false)
 	this.checked = true;
 }));
 
 
+
 $form.addEventListener('submit', async function (e) {
 
-	e.preventDefault();
+	try {
 
-	// Prevent multiple clicks while loading
-	toggleCompareButton();
+		e.preventDefault();
 
-	let [smoothScroll, isURL, imagesLoaded] = await Promise.all([import('scroll-to-element'), import('validator/lib/isURL'), import('images-loaded')]);
+		// Prevent multiple clicks while loading
+		toggleCompareButton();
 
-	smoothScroll = smoothScroll.default;
-	isURL = isURL.default;
-	imagesLoaded = imagesLoaded.default;
+		let [smoothScroll, isURL, imagesLoaded] = await Promise.all([import('scroll-to-element'), import('validator/lib/isURL'), import('images-loaded')]);
 
-	const cannonicalURL = this.cannonicalURL.value;
+		smoothScroll = smoothScroll.default;
+		isURL = isURL.default;
+		imagesLoaded = imagesLoaded.default;
 
-	const ampURL = this.ampURL.value;
+		const cannonicalURL = this.cannonicalURL.value;
 
-	if (!isURL(this.cannonicalURL.value)) {
-		errorMsg('The Cannonical URL is not a valid URL', 'error')
-		return false;
-	}
+		const ampURL = this.ampURL.value;
 
-	if (!isURL(this.ampURL.value)) {
-		errorMsg('The AMP URL is not a valid URL', 'error')
-		return false;
-	}
+		if (!isURL(this.cannonicalURL.value)) {
+			errorMsg('The Cannonical URL is not a valid URL', 'error')
+			return false;
+		}
 
-	addLoader();
+		if (!isURL(this.ampURL.value)) {
+			errorMsg('The AMP URL is not a valid URL', 'error')
+			return false;
+		}
 
-	console.log()
-	const $iframeContainer = document.querySelector('.iframe-container');
+		addLoader();
 
-	//Clear existing comparisons
-	$iframeContainer.innerHTML = "";
+		const $iframeContainer = document.querySelector('.iframe-container');
 
-	// insert the headers 
-	$iframeContainer.insertAdjacentHTML('afterbegin', `
+		//Clear existing comparisons
+		$iframeContainer.innerHTML = "";
 
-	<h2 class="cannonical"> Canonical </h2> 
-				
-	<h2 class="amp"> AMP </h2>
+		// insert the headers 
+		$iframeContainer.insertAdjacentHTML('afterbegin', `
 	
-	`);
+		<h2 class="cannonical"> Canonical </h2> 
+					
+		<h2 class="amp"> AMP </h2>
+		
+		`);
 
 
-	if (this.forceIframeMode.checked) {
+		if (this.forceIframeMode.checked) {
 
-		$iframeContainer.insertAdjacentHTML('beforeend', `<iframe id="iframe-amp" class="u-shadowS" width="412" height="12000" src="https://iframereplacement.speedwat.ch/?url=${this.ampURL.value}" frameborder="0"></iframe>`);
-		$iframeContainer.insertAdjacentHTML('beforeend', ` <iframe id="iframe-canonical" class="u-shadowS" width="412" height="12000" src="https://iframereplacement.speedwat.ch/?url=${this.cannonicalURL.value}" frameborder="0"></iframe> `);
+			$iframeContainer.insertAdjacentHTML('beforeend', `<iframe id="iframe-amp" class="u-shadowS" width="412" height="12000" src="https://iframereplacement.speedwat.ch/?url=${this.ampURL.value}" frameborder="0"></iframe>`);
+			$iframeContainer.insertAdjacentHTML('beforeend', ` <iframe id="iframe-canonical" class="u-shadowS" width="412" height="12000" src="https://iframereplacement.speedwat.ch/?url=${this.cannonicalURL.value}" frameborder="0"></iframe> `);
 
-		errorMsg(`We are forcing the site to display inside iFrames ! <strong>This can lead to visual differences</strong>`, 'warning');
-	}
-
-
-	if (this.screenshotsOnlyMode.checked) {
-
-		$iframeContainer.insertAdjacentHTML('beforeend', `<img id="iframe-canonical" class="u-shadowS" width="412px" src="https://puppeteer.speedwat.ch/screenshot/?url=${this.cannonicalURL.value}" />`);
-		$iframeContainer.insertAdjacentHTML('beforeend', `<img id="iframe-amp" class="u-shadowS" width="412px" src="https://puppeteer.speedwat.ch/screenshot/?url=${this.ampURL.value}" />`);
-
-		errorMsg(`You are in screenshot only mode `, 'success');
-
-		$iframeContainer.style.visibility = "hidden";
-		await imagesLoaded('.iframe-container');
-		$iframeContainer.style.visibility = "visible";
-	}
+			errorMsg(`We are forcing the site to display inside iFrames ! <strong>This can lead to visual differences</strong>`, 'warning');
+		}
 
 
+		if (this.screenshotsOnlyMode.checked) {
 
-	if (this.autoMode.checked) {
-
-		const {
-			cannonicalCanBeIframed,
-			ampCanBeIframed
-		} = await testIframes(this.cannonicalURL.value, this.ampURL.value).catch(console.log)
-
-		if (cannonicalCanBeIframed) {
-			// check if the cannonicalCanBeIframed 
-			$iframeContainer.insertAdjacentHTML('beforeend', ` <iframe id="iframe-canonical" class="u-shadowS" width="412" height="12000" src="${this.cannonicalURL.value}" frameborder="0"></iframe> `);
-		} else {
-			// else use a screenshot 
 			$iframeContainer.insertAdjacentHTML('beforeend', `<img id="iframe-canonical" class="u-shadowS" width="412px" src="https://puppeteer.speedwat.ch/screenshot/?url=${this.cannonicalURL.value}" />`);
-		}
-
-
-		if (ampCanBeIframed) {
-			$iframeContainer.insertAdjacentHTML('beforeend', `<iframe id="iframe-amp" class="u-shadowS" width="412" height="12000" src="${this.ampURL.value}" frameborder="0"></iframe>`);
-		} else {
 			$iframeContainer.insertAdjacentHTML('beforeend', `<img id="iframe-amp" class="u-shadowS" width="412px" src="https://puppeteer.speedwat.ch/screenshot/?url=${this.ampURL.value}" />`);
-		}
 
+			errorMsg(`You are in screenshot only mode `, 'success');
 
-		if (!cannonicalCanBeIframed || !ampCanBeIframed) {
-
-			let errorDetail = "";
-
-			if (!cannonicalCanBeIframed) {
-				errorDetail = "for the cannonical page only";
-			}
-
-			if (!ampCanBeIframed) {
-				errorDetail = "for the ⚡️ amp page only";
-			}
-
-			if (!cannonicalCanBeIframed && !ampCanBeIframed) {
-				errorDetail = "for both pages";
-			}
-
-			errorMsg(`The site blocked iframes ! We are falling back on screenshots <strong>${errorDetail}</strong>!`, 'warning');
 			$iframeContainer.style.visibility = "hidden";
 			await imagesLoaded('.iframe-container');
 			$iframeContainer.style.visibility = "visible";
 		}
 
 
+
+		if (this.autoMode.checked) {
+
+			const {
+				cannonicalCanBeIframed,
+				ampCanBeIframed
+			} = await testIframes(this.cannonicalURL.value, this.ampURL.value).catch(console.log)
+
+			if (cannonicalCanBeIframed) {
+				// check if the cannonicalCanBeIframed 
+				$iframeContainer.insertAdjacentHTML('beforeend', ` <iframe id="iframe-canonical" class="u-shadowS" width="412" height="12000" src="${this.cannonicalURL.value}" frameborder="0"></iframe> `);
+			} else {
+				// else use a screenshot 
+				$iframeContainer.insertAdjacentHTML('beforeend', `<img id="iframe-canonical" class="u-shadowS" width="412px" src="https://puppeteer.speedwat.ch/screenshot/?url=${this.cannonicalURL.value}" />`);
+			}
+
+
+			if (ampCanBeIframed) {
+				$iframeContainer.insertAdjacentHTML('beforeend', `<iframe id="iframe-amp" class="u-shadowS" width="412" height="12000" src="${this.ampURL.value}" frameborder="0"></iframe>`);
+			} else {
+				$iframeContainer.insertAdjacentHTML('beforeend', `<img id="iframe-amp" class="u-shadowS" width="412px" src="https://puppeteer.speedwat.ch/screenshot/?url=${this.ampURL.value}" />`);
+			}
+
+
+			if (!cannonicalCanBeIframed || !ampCanBeIframed) {
+
+				let errorDetail = "";
+
+				if (!cannonicalCanBeIframed) {
+					errorDetail = "for the cannonical page only";
+				}
+
+				if (!ampCanBeIframed) {
+					errorDetail = "for the ⚡️ amp page only";
+				}
+
+				if (!cannonicalCanBeIframed && !ampCanBeIframed) {
+					errorDetail = "for both pages";
+				}
+
+				errorMsg(`The site blocked iframes ! We are falling back on screenshots <strong>${errorDetail}</strong>!`, 'warning');
+				$iframeContainer.style.visibility = "hidden";
+				await imagesLoaded('.iframe-container');
+				$iframeContainer.style.visibility = "visible";
+			}
+
+
+		}
+
+		removeLoader();
+		toggleCompareButton();
+
+		smoothScroll('.iframe-container', {
+			offset: -200,
+			duration: 700
+		});
+
+
+		window.location.hash = `?ampURL=${ampURL}&cannonicalURL=${cannonicalURL}`;
+
+
+	} catch (e) {
+
+		errorMsg(`Something wrong happened : ${e.toString()}`, 'error');
+
 	}
 
-	removeLoader();
-	toggleCompareButton();
-
-	smoothScroll('.iframe-container', {
-		offset: -200,
-		duration: 700
-	});
-
-
-	window.location.hash = `?ampURL=${ampURL}&cannonicalURL=${cannonicalURL}`;
-
 });
+
 
 $form.cannonicalURL.addEventListener('change', async function (e) {
-	prepareDetection(this, $form.ampURL, 'amphtml');
+	tryToGetAlternateURL(this.value, $form.ampURL, 'amphtml').catch(console.log)
 });
+
+
 $form.ampURL.addEventListener('change', async function (e) {
-	prepareDetection(this, $form.cannonicalURL, 'canonical');
+	tryToGetAlternateURL(this.value, $form.cannonicalURL, 'canonical').catch(console.log)
 });
+
 
 if (window.location.hash !== "" && window.location.hash.includes('ampURL') && window.location.hash.includes('cannonicalURL')) {
 
